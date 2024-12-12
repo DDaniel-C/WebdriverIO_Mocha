80a74e6df6f7299f213877344d751828b8e4b266
DDaniel-C <J@maica123->
2024-12-12 23:24:12 +0000

    cleaned up code

--- test/specs/products.js
+++ test/specs/products.js
@@ -13,39 +13,27 @@ describe('Sorting out the products', () => {
     })
 
     it('should filter products by price (high to low)', async () => {
-        // Ensure the products title is displayed
-        await expect(productPage.productsTitle).toBeDisplayed();
-        // Validate the title text
-        await expect(productPage.productsTitle).toHaveText('Products');
-        // Select the filter option for "High to Low" prices
-        await productPage.filterOptions.selectByAttribute("value", 'hilo');
-        console.log(await productPage.filterOptions.getValue());
-        // Get and parse the first product's price
-        const firstPriceText = await productPage.firstPrice.getText();
-        const firstPrice = parseInt(firstPriceText.replace('$', ''), 10);
-        // Get and parse the last product's price
-        const lastPriceText = await productPage.lastPrice.getText();
-        const lastPrice = parseInt(lastPriceText.replace('$', ''), 10);
-        // Verify the first price is greater than the last price
-        await expect(firstPrice).toBeGreaterThan(lastPrice);
+        await expect(productPage.productsTitle).toBeDisplayed()
+        await expect(productPage.productsTitle).toHaveText('Products')
+        await productPage.filterOptions.selectByAttribute("value", 'hilo')
+        console.log(await productPage.filterOptions.getValue())
+        const firstPriceText = await productPage.firstPrice.getText()
+        const firstPrice = parseInt(firstPriceText.replace('$', ''), 10)
+        const lastPriceText = await productPage.lastPrice.getText()
+        const lastPrice = parseInt(lastPriceText.replace('$', ''), 10)
+        await expect(firstPrice).toBeGreaterThan(lastPrice)
     })
 
     it('should filter products by price (low to high)', async () => {
-        // Ensure the products title is displayed
-        await expect(productPage.productsTitle).toBeDisplayed();
-        // Validate the title text
-        await expect(productPage.productsTitle).toHaveText('Products');
-        // Select the filter option for "Low to High" prices
-        await productPage.filterOptions.selectByAttribute("value", 'lohi');
-        console.log(await productPage.filterOptions.getValue());
-        // Get and parse the first product's price
-        const firstPriceText = await productPage.firstPrice.getText();
-        const firstPrice = parseInt(firstPriceText.replace('$', ''), 10);
-        // Get and parse the last product's price
-        const lastPriceText = await productPage.lastPrice.getText();
-        const lastPrice = parseInt(lastPriceText.replace('$', ''), 10);
-        // Verify the first price is less than the last price
-        await expect(firstPrice).toBeLessThan(lastPrice);
+        await expect(productPage.productsTitle).toBeDisplayed()
+        await expect(productPage.productsTitle).toHaveText('Products')
+        await productPage.filterOptions.selectByAttribute("value", 'lohi')
+        console.log(await productPage.filterOptions.getValue())
+        const firstPriceText = await productPage.firstPrice.getText()
+        const firstPrice = parseInt(firstPriceText.replace('$', ''), 10)
+        const lastPriceText = await productPage.lastPrice.getText()
+        const lastPrice = parseInt(lastPriceText.replace('$', ''), 10)
+        await expect(firstPrice).toBeLessThan(lastPrice)
     });
 
 
